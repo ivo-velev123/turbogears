541126fa4db7954bda03f1959f7d265dc931cf39
Ivo-velev <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>
2025-07-11 13:48:37 +0100

    initial commit

---
+++ tgapp.py
@@ -0,0 +1,36 @@
+from tg import expose, TGController, MinimalApplicationConfigurator
+from tg.configurator.components.statics import StaticsConfigurationComponent
+from wsgiref.simple_server import make_server
+import webhelpers2
+import webhelpers2.text
+
+class RootController(TGController):
+    @expose('index.xhtml')
+    def index(self, person=None):
+        return dict(person=person)
+    
+    @expose('hello.xhtml')
+    def hello(self, person=None):
+        return dict(person=person)
+    
+config = MinimalApplicationConfigurator()
+config.update_blueprint({
+    'server_static': True,
+    'root_controller': RootController(),
+    'renderers': ['kajiki'],
+    'helpers': webhelpers2
+})
+
+config.register(StaticsConfigurationComponent)
+config.update_blueprint({
+    'serve_static': True,
+    'paths': {
+        'static_files': 'public'
+    }
+})
+    
+application = config.make_wsgi_app()
+
+print("Serving on port 8080...")
+httpd = make_server('', 8080, application)
+httpd.serve_forever()
