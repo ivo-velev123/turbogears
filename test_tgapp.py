66da619829104d5bac91fdb09248dbeeec2b0dec
Ivo-velev <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>
2025-07-16 15:29:59 +0100

    added a test for templating engine

--- test_tgapp.py
+++ test_tgapp.py
@@ -1,1 +1,30 @@
-from 
+import kajiki
+
+
+def test_index_template():
+    with open("index.xhtml", "r") as f:
+        lines = f.readlines()
+
+    output = "".join(lines)
+
+    template = kajiki.XMLTemplate(output)
+    print(template(dict(person="ivo")).render())
+    comparison = "".join(template(dict(person="ivo")).render())
+
+    assert """<html>
+    <body>
+        <header>
+            
+                <h1>ivo's HTML Website practice</h1>
+            
+        </header>
+        <article>
+            <h3 class="left">Here is a list of things you can do with HTML5:</h3>
+            <ul class="left">
+                <li><a href="html5Elements.html">These are a few elements.</a></li>
+                <li><a href="video.html">This is a video embedded into html</a></li>
+                <li><a href="audio.html">This is audio embedded into html</a></li>
+            </ul>
+        </article>
+    </body>
+</html>""" == comparison
